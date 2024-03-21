6347a7c9c86f26a88bba3fe6b60d0dc018101587
Abdullahil Ashik Md. Arefin <abdullah.ashik.arefin@gmail.com>
2024-03-21 20:10:51 +0600

    starter

---
+++ src/constrant/index.js
@@ -0,0 +1,234 @@
+export const homeTownImages = [
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-title-Dhaka.jpg?width=720",
+        "title": "Dhaka"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-title-Chittagong.jpg?width=720",
+        "title": "Chittagong"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Bagerhat.jpg?width=720",
+        "title": "Bagerhat"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Bandarban.jpg?width=720",
+        "title": "Bandarban"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Barguna.jpg?width=720",
+        "title": "Barguna"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Barisal.jpg?width=720",
+        "title": "Barisal"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Bhola.jpg?width=720",
+        "title": "Bhola"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Bogra.jpg?width=720",
+        "title": "Bogra"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Brahmanbaria.jpg?width=720",
+        "title": "Brahmanbaria"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Chandpur.jpg?width=720",
+        "title": "Chandpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Chuadanga.jpg?width=720",
+        "title": "Chuadanga"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-cumilla.jpg?width=720",
+        "title": "Comilla"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-title-Cox's%20Bazaar.jpg?width=720",
+        "title": "Cox's Bazar"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Dinajpur.jpg?width=720",
+        "title": "Dinajpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Faridpur.jpg?width=720",
+        "title": "Faridpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-feni.jpg?width=720",
+        "title": "Feni"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Gaibandha.jpg?width=720",
+        "title": "Gaibandha"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Tongi.jpg?width=720",
+        "title": "Gazipur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Gopalganj.jpg?width=720",
+        "title": "Gopalganj"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Jamalpur.jpg?width=720",
+        "title": "Jamalpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Jessore.jpg?width=720",
+        "title": "Jessore"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Jhenaidha.jpg?width=720",
+        "title": "Jhenaidah"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Keraniganj.jpg?width=720",
+        "title": "Keraniganj"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Khulna.jpg?width=720",
+        "title": "Khulna"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Kishoreganj.jpg?width=720",
+        "title": "Kishoreganj"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Kurigram.jpg?width=720",
+        "title": "Kurigram"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Kushtia.jpg?width=720",
+        "title": "Kushtia"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Lakshmipur.jpg?width=720",
+        "title": "Lakshmipur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Madaripur.jpg?width=720",
+        "title": "Madaripur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Manikganj.jpg?width=720",
+        "title": "Manikganj"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Meherpur.jpg?width=720",
+        "title": "Meherpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Moulvibazar.jpg?width=720",
+        "title": "Moulvibazar"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Munshiganj.jpg?width=720",
+        "title": "Munshiganj"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Mymensingh.jpg?width=720",
+        "title": "Mymensingh"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Naogaon.jpg?width=720",
+        "title": "Naogaon"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Narsingdi.jpg?width=720",
+        "title": "Narsingdi"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Natore.jpg?width=720",
+        "title": "Natore"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Nilphamari.jpg?width=720",
+        "title": "Nilphamari"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-noakhali.jpg?width=720",
+        "title": "Noakhali"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Pabna.jpg?width=720",
+        "title": "Pabna"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Pirojpur.jpg?width=720",
+        "title": "Pirojpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Rajshahi.jpg?width=720",
+        "title": "Rajshahi"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Rangpur.jpg?width=720",
+        "title": "Rangpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Satkhira.jpg?width=720",
+        "title": "Satkhira"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Savar.jpg?width=720",
+        "title": "Savar"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Shariatpur.jpg?width=720",
+        "title": "Shariatpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Sherpur.jpg?width=720",
+        "title": "Sherpur"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Sirajganj.jpg?width=720",
+        "title": "Sirajganj"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-title-Sylhet.jpg?width=720",
+        "title": "Sylhet"
+    },
+    {
+        "src": "https://images.deliveryhero.io/image/fd-bd/city-title/city-tile-Tangail.jpg?width=720",
+        "title": "Tangail"
+    }
+];
+
+export const countryList = ['Myanmar', 'Laos','Hong Kong','Pakistan','Singapore','Malaysia','Philippines','Thailand','Taiwan','Cambodia'];
+export const link1 = [
+    {title: 'Press',link: '##'},
+    {title: 'Privacy Policy',link: '##'},
+    {title: 'Panda Pro Subscription',link: '##'},
+    {title: 'Partner with Us',link: '##'},
+    {title: 'Careers',link: '##'},
+    {title: 'Pandago for Customers',link: '##'},
+    {title: 'Become an Affiliate',link: '##'},
+];
+
+export const link2 = [
+    {title: 'Help Center', link: '##'},
+    {title: 'Refund Account Terms and Conditions', link: '##'},
+    {title: 'Dine-in', link: '##'},
+    {title: 'How foodpanda Works', link: '##'},
+    {title: 'Corporate Customer', link: '##'},
+    {title: 'Foodpanda Deals', link: '##'},
+    {title: 'All Cities', link: '##'},
+];
+
+export const link3 = [
+    {title: 'Terms and Conditions', link: '##'},
+    {title: 'Cashback Program', link: '##'},
+    {title: 'Security', link: '##'},
+    {title: 'Download Foodpanda Apps', link: '##'},
+    {title: 'pandaGo for Business', link: '##'},
+    {title: 'Grocery Delivery', link: '##'},
+    {title: 'Ramadan Delivery', link: '##'},
+];
+
